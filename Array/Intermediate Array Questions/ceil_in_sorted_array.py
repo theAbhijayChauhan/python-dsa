226b76bb8c55193d217e1f84820a2ca26728173a
theAbhijayChauhan <abhijaychauhan99@gmail.com>
2026-07-29 19:59:57 +0530

    Find Ceil of a number in a sorted array

--- Array/Intermediate Array Questions/ceil_in_sorted_array.py
+++ Array/Intermediate Array Questions/ceil_in_sorted_array.py
@@ -1,1 +1,17 @@
 # Problem : The ceil of a number is the smallest element greater than or equal to the target.
+
+def ceil_element(arr, target):
+    low = 0
+    high = len(arr) -1
+    answer = -1
+    while low <= high:
+        mid = (high + low) // 2
+        if arr[mid] >= target:
+            answer = arr[mid]
+            high = mid - 1
+        else:
+            low = mid + 1
+    return answer
+
+arr = [2, 5, 8, 12, 16]
+print(ceil_element(arr, 10))
