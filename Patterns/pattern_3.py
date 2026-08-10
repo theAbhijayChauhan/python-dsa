af4203c97db1d476de16b449c3b0ed53047359ab
theAbhijayChauhan <abhijaychauhan99@gmail.com>
2026-08-10 20:38:55 +0530

    Pattern problem 3

---
+++ Patterns/pattern_3.py
@@ -0,0 +1,15 @@
+""" print 1
+          1 2
+          1 2 3
+          1 2 3 4   
+          1 2 3 4 5   
+"""
+
+def pattern(num):
+    for i in range(num):
+        for j in range(i + 1):
+            print(j+1, end = " ")
+        print()
+
+num = int(input("Enter the number: "))
+pattern(num)
